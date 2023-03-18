c073b9a81133f6dc3f96a62e986f50fd72c4fd46
ImboJ <pheliximbo08@gmail.com>
2023-03-18 05:15:44 +0000

    The last digit assignment resubmitted correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -33,7 +33,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, n_last);
 	}
-	else if (n == 0)
+	else if (n_last == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n_last);
 	}
